72037b4ac5fd062ae95f007a71d8410fb99bd61e
Gabriela Bazan <gabrielabazan11@gmail.com>
2021-01-28 15:00:31 +0100

    eval blanch

---
+++ main.js
@@ -0,0 +1,39 @@
+// Change the border color of the form when submit if the field is empty
+
+$("#mySelectBox");
+
+$("#myTextarea");
+
+console.log($("button"));
+
+$("button").on("click", (e) => {
+  e.preventDefault();
+  $("#mySelectBox").removeClass("empty");
+  $("#myTextarea").removeClass("empty");
+
+  console.log($("#mySelectBox").val());
+  console.log($("#myTextarea").val());
+  console.log($("#myTextarea").val().length);
+
+  if ($("#mySelectBox").val() === "") {
+    $("#mySelectBox").addClass("empty");
+  }
+
+  if ($("#myTextarea").val().length < 20) {
+    $("#myTextarea").addClass("empty");
+  }
+});
+
+//--------------- Cat animation --------------
+
+console.log($("footer img"));
+
+$("footer img").hover(function (e) {
+  $("footer img").toggleClass("cat-change");
+  //$("footer img").off("mouseenter mouseleave");
+});
+
+// $("button").on("mouseover", function (e) {
+//   $(e.target).css("color", "green");
+//   $("button").off("mouseover");
+// });
